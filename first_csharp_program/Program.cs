60488bb6a7eac7855c0d97f14e286ac3a4bfd429
Goosebagel <goosebagelftw@gmail.com>
2014-02-19 18:54:21 -0800

    Yea
    This is epic!!!!!

---
+++ first_csharp_program/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   
+            int x = 7;
+            
+            Console.WriteLine("x equals {0}", x);
+
+            x += x;
+            
+            Console.WriteLine("x now equals {0}", x);
+
+            x /= x;
+
+            Console.WriteLine("x now equals {0}", x);
+
+            Console.WriteLine("We are so good \n at this math stuff");
+
+            Console.WriteLine(@"This    is 
+some    wierd 
+text
+now!    !");
+
+            
+            Console.Write(x > 9 && x < 283);
+
+
+            Console.Write("\n I'm too good");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
